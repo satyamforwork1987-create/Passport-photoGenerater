6aa8007379c7c1564273c71ffa960134b55cb133
satyam-0002 <satyamforwork1987@gmail.com>
2026-08-08 10:46:38 +0530

    Initial commit

--- app.py
+++ app.py
@@ -210,6 +210,11 @@ def health():
     return jsonify({'status': 'ok'})
 
 
+@app.route('/ping')
+def ping():
+    return 'pong', 200
+
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
